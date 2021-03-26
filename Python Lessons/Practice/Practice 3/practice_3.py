213f34bf53e590556a6a695c94372288f68dca2c
axisrin <nonameanime4@mail.ru>
2021-03-26 14:43:40 +0300

    added first exercise

---
+++ Python Lessons/Practice/Practice 3/practice_3.py
@@ -0,0 +1,27 @@
+def first():
+
+    x=1
+    # expected 2 blank lines, found 1
+    # def func1():
+    #     pass
+    # def func2():
+    #     pass
+
+    # multiple statements on one line (colon)
+    # if a > 2: b = 10
+
+    #multiple statements on one line (semicolon).
+    #from gevent import gunga; gunga.patch_all()
+
+    # comparison to None should be 'if cond is None:'.
+    # if var != True:
+    # print("var is not equal to True")
+    # if var == None:
+    # print("var is equal to None")
+
+    # comparison to True should be 'if cond is True:' or 'if cond:'
+    # x = True
+    # if x == True:
+    # print('True!')
+
+first()
